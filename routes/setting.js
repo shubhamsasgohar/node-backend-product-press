e59172ab9e8aa319c0616dab5dd364d07bf981fd
shubham <shubhamsasgohar@gmail.com>
2024-03-28 17:37:07 +0530

    changes done

--- routes/setting.js
+++ routes/setting.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const defalut = require("../controller/Defalut.js");
-
+const defalut = require("../controller/Defalut");
 
 const router = express.Router();
 router.post("/createSetting", defalut.createSetting)
